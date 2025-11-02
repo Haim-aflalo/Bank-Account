29d3550f0de32e2a596eed57c15e777865d331d5
Haim Aflalo <kodkodhaim@gmail.com>
2025-11-02 11:26:09 +0200

    BankAccount class and init method

---
+++ BankAccount.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    """
+    BankAccount
+    """
+
+    def __init__(self,account_holder,initial_balance):
+        self.holder = account_holder
+        self.balance = initial_balance
+
+
+
+
